86503d5dca141d8a1d83b0985b4e4bb7e4dd29d0
meltstudio-git <meltstudio666@gmail.com>
2026-07-10 03:19:20 +0200

    v26.8: sube versión y cache-bust tras el fix de El Despojo (PR #32)

--- game/js/main.js
+++ game/js/main.js
@@ -1,7 +1,7 @@
 // Arranque: input, bucle de animación y pantalla de título.
 (function () {
   // versión visible del juego (Ajustes); súbela con cada tanda de cambios
-  window.VERSION_JUEGO = 'v26.7';
+  window.VERSION_JUEGO = 'v26.8';
   const world = Game.world;
   world.data = window.GAME_DATA;
 
